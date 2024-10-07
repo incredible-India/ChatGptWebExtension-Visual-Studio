278c4f8ad267823a7b883f0f0fb336dd94fd092e
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-10-07 17:13:59 +0530

    extension icon and code added

---
+++ ChatGptWeb/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatGptWeb
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Chatgpt Window";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("2f799c97-e4ff-475a-a083-7878b49d6677")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ ChatGptWeb/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+namespace ChatGptWeb
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        public MyToolWindowControl()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            Random rnd = new Random();
+            string subFolder = rnd.Next().ToString();
+            var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync(userDataFolder: Path.Combine(Path.GetTempPath(), $"{Environment.UserName}", subFolder));
+            await YouTubeWebView.EnsureCoreWebView2Async(env);
+            var request = YouTubeWebView.CoreWebView2.Environment.CreateWebResourceRequest("https://www.Chatgpt.com", "GET", null, null);
+            YouTubeWebView.CoreWebView2.NavigateWithWebResourceRequest(request);
+        }
+    }
+}
